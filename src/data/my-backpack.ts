41ed6cf890040a00b2ee736201244f06e759d38a
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 23:05:07 +0530

    Update my-backpack.ts

--- src/data/my-backpack.ts
+++ src/data/my-backpack.ts
@@ -17,9 +17,9 @@ export const myBackpack: [string, string[]][] = [
       
     ],
   ],
-  ["Backend", ["Papon Lapate, Thailand", "Aritra Mondal, India", "Gabriel, Singapore", 
+  [" ", ["Papon Lapate, Thailand", "Aritra Mondal, India", "Gabriel, Singapore", 
                "Arkan Manva, India" , "Vlad Spataru, Romania" , "Vedant Saini, India" ,
                "Mattedy" ,"Valentino Iversion, Indonesia" ,"Anand, India" ,"NJOY, India" ,"Sunaina Pati, India" ]],
   
-  ["Other", ["Orestis Lignos, Greece", "Jimin Kim, Korea","Evan Chang", "Krishna Prothapragada, USA", "k12byda5h, Thailand", "Andre Luis, Bolivia", "navi_09220114, Malaysia", "Justin Lee, USA", "Rafael Yanto", "Miquel-point", "player_01"]],
+  [" ", ["Orestis Lignos, Greece", "Jimin Kim, Korea","Evan Chang", "Krishna Prothapragada, USA", "k12byda5h, Thailand", "Andre Luis, Bolivia", "navi_09220114, Malaysia", "Justin Lee, USA", "Rafael Yanto", "Miquel-point", "player_01"]],
 ];
