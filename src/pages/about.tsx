a1870a615c04a8bf535e8858cee07a872ba8c09a
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 16:43:02 +0530

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,18 +1,15 @@
 import { NextSeo } from "next-seo";
 import Link from "next/link";
-import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { Age } from "components/Age";
 import { Layout } from "components/Layout";
 import { GetStaticProps } from "next";
 import { TimelineItem } from "types/Timeline";
 import { Timeline } from "components/timeline/Timeline";
 
-const pageTitle = "About - Casper Iversen";
-const pageDescription = "Get to know more about me and some of my accomplishments.";
+const pageTitle = "About - GIMO";
+const pageDescription = "About GIMO";
 
-export default function About({ timelineData }: { timelineData: TimelineItem[] }) {
-  // this is roughly the date I started programing according to GitHub 😅!
-  const started = new Date("2019-08-08");
+
 
   return (
     <Layout>
@@ -24,14 +21,25 @@ export default function About({ timelineData }: { timelineData: TimelineItem[] }
       />
 
       <section id="about">
-        <h1 className="section-title">About Me</h1>
+        <h1 className="section-title">About GIMO</h1>
 
         <div className="max-w-3xl mt-5 text-neutral-800 dark:text-gray-200">
           <p>
-            Hello, I am Casper! {"I'm"} a <Age />
-            -year-old programmer and student based in Belgium. I adore building accessible,
-            responsive and fast code. Furthermore, {"I'm"} also a big fan of open-source, I
-            contribute to open-source as much as I can, I also have{" "}
+           GIMO is an International Mathematical Olympiad Style mathematical contest held online
+            for individuals. It is similar to the IMO format except that here the contest will be individual based rather than team based and it will be untimed.
+            You will be given nearly twelve days to work out with six problems and you have to submit them within that time interval. 
+            <br></br>
+            
+             <h1 className="section-title">Contest Schedule</h1>
+            
+            
+            <br> The contest will be tentatively held on August last week or september first week.
+            </br>
+            
+           <br></br> The team which brings out GIMO is the GIMO team which consists of multiple IMO contestants who have
+            won gold, silver, bronze and honourable mentions in IMO for their country, some who are going to partcipate 
+            in IMO this year representing their country, MOP ers, USA(J)MO qualifiers and math enthusiasts.
+            Here yTo know more, check the about page.
             <a
               target="_blank"
               rel="noopener noreferrer"
