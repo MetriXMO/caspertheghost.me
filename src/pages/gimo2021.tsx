22d16f68cfd109d266434600f94447df47c963ab
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 20:58:11 +0530

    Update gimo2021.tsx

--- src/pages/gimo2021.tsx
+++ src/pages/gimo2021.tsx
@@ -2,7 +2,7 @@ import { Project } from "types/Project";
 import { Layout } from "components/Layout";
 import { GetStaticProps } from "next";
 import { FeaturedProjectsSection } from "components/FeaturedProjects";
-import { Project } from "types/Project";
+
 
 interface Props {
   projects: Project[];
