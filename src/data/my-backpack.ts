6160456dc68660717783d0631f49df3efd823dc4
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 12:34:44 +0530

    Update my-backpack.ts

--- src/data/my-backpack.ts
+++ src/data/my-backpack.ts
@@ -1,16 +1,25 @@
 export const myBackpack: [string, string[]][] = [
   [
-    "Frontend",
+    "Team Members",
     [
-      "HTML, CSS & SCSS",
-      "JavaScript",
-      "TypeScript",
-      "React & Next.js",
-      "styled-components",
-      "Tailwind CSS",
-      "State Management",
+      "Kaixin Wang, Canda",
+      "Agamjeet Singh, India",
+      "Bora Olmez, Switzerland",
+      "Atul Nadig, India",
+      "Fedir Yudin, Ukraine",
+      "Andrei Chirita, Romania",
+      "Tiger Zhang, USA",
+      "Archit Manas, India"
+      "Vivian Loh, USA"
+      "Samarth, India"
+      "Ryan Shin, USA"
+      "player_01"
+      
     ],
   ],
-  ["Backend", ["Node", "REST APIs", "MongoDB & MySQL", "Prisma"]],
-  ["Other", ["Git & GitHub", "NGINX", "DigitalOcean", "VS Code", "Docker"]],
+  ["Backend", ["Papon Lapate, Thailand", "Aritra Mondal, India", "Gabriel, Singapore", 
+               "Arkan Manva, India" , "Vlad Spataru, Romania" , "Vedant Saini, India" ,
+               "Mattedy" ,"Valentino Iversion" ,"Anand, India" ,"NJOY, India" ,"Sunaina Pati, India" ,"Justin Lee"  ]],
+  
+  ["Other", ["Orestis Lignos, Greece", "Jimin Kim, Korea","Evan Chang", "Krishna Prothapragada, USA", "Andre Luis, Bolivia", "navi_09220114"]],
 ];
