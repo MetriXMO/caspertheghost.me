51db1e2a05905dd3f721258b302cc134e657d2df
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 11:14:33 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,24 +8,17 @@ export const HeroSection = () => {
   return (
     <section className="flex flex-col justify-center mx-auto min-h-[40em]" id="top">
       <div>
-        <h4 className="text-2xl sm: md:text-3xl mb-3 text-neutral-700 dark:text-[#CDD6E2]">
-          <span className="inline-block animate-wave" aria-label="Waving" role="img">
-            👋
-          </span>{" "}
-          Hello! I am
-        </h4>
         <h1 className="text-5xl font-bold sm:text-6xl md:text-7xl lg:text-8xl">
-          Wasper<span className="hidden xs:inline"> Iversen</span>,
+          GIMO<span className="hidden xs:inline">2022</span>,
         </h1>
         <h2 className="mt-2 text-4xl font-medium sm:mt-5 sm:text-4xl md:text-5xl lg:text-6xl">
-          and {"I'm"} a web developer.
+          Online Math Conest, Year 2
         </h2>
       </div>
 
       <p className="max-w-3xl mt-8 text-neutral-800 dark:text-gray-300 md:text-xl">
-        I am an extremely motivated programmer and student from Belgium. {"I'm"} a self-taught
-        developer and have been programming for {formatDistanceToNow(started)} and {"I'm"} still
-        learning new technologies every week, mostly focusing on frontend related technologies.{" "}
+        GIMO is an International Mathematical Olympiad-style mathematical contest held online for individuals 
+        where you will be given nearly 10 days work out the roblems and submit them within that time interval. Toknow more,check the about page.
         <Link href="/about">
           <a className="font-medium underline">Read more.</a>
         </Link>
