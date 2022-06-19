6040273e385ae2ecb55d346e174d6e5aac0ea5ab
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 17:16:21 +0530

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,18 @@
 import { NextSeo } from "next-seo";
 import Link from "next/link";
-
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { Age } from "components/Age";
 import { Layout } from "components/Layout";
 import { GetStaticProps } from "next";
 import { TimelineItem } from "types/Timeline";
 import { Timeline } from "components/timeline/Timeline";
 
-const pageTitle = "About - GIMO";
-const pageDescription = "Get to know more about gimo";
+const pageTitle = "About - Casper Iversen";
+const pageDescription = "Get to know more about me and some of my accomplishments.";
 
-
+export default function About({ timelineData }: { timelineData: TimelineItem[] }) {
+  // this is roughly the date I started programing according to GitHub 😅!
+  const started = new Date("2019-08-08");
 
   return (
     <Layout>
@@ -21,23 +24,27 @@ const pageDescription = "Get to know more about gimo";
       />
 
       <section id="about">
-            <h1 className="section-title">Information about the contest and Schedule of the contest</h1>
+        <h1 className="section-title">About Me</h1>
 
         <div className="max-w-3xl mt-5 text-neutral-800 dark:text-gray-200">
           <p>
-            GIMO is an International Mathematical Olympiad Style mathematical contest held online
-            for individuals. It is similar to the IMO format except that here the contest will be individual based rather than team based and it will be untimed.
-            You will be given nearly twelve days to work out with six problems and you have to submit them within that time interval. 
-           
+            Hello, I am Casper! {"I'm"} a <Age />
+            -year-old programmer and student based in Belgium. I adore building accessible,
+            responsive and fast code. Furthermore, {"I'm"} also a big fan of open-source, I
+            contribute to open-source as much as I can, I also have{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black dark:text-white underline"
+              href="https://github.com/dev-caspertheghost?tab=repositories"
+            >
+              many open-source projects
+            </a>
+            .
           </p>
           <p className="mt-5">
-            The team which brings out GIMO is the GIMO team which consists of multiple IMO contestants who have
-            won gold, silver, bronze and honourable mentions in IMO for their country, some who are going to partcipate 
-            in IMO this year representing their country, MOP ers, USA(J)MO qualifiers and math enthusiasts.
-          </p>
-           <p className="mt-5">
             I have been developing web applications, Discord bots and npm packages for{" "}
-            and love it! I learn something new almost every day!
+            {formatDistanceToNow(started)} and love it! I learn something new almost every day!
             Currently, {"I'm"} focusing on frontend web development, specifically working with
             React.js, TypeScript, CSS, HTML and much more!
           </p>
@@ -48,10 +55,14 @@ const pageDescription = "Get to know more about gimo";
           <p className="mt-5 text-base italic">
             PS: If there is something that {"you'd"} like to know more about me, {"don't"} hesitate
             to{" "}
-           
-    </p>
+            <Link href="/#contact">
+              <a className="underline">contact me</a>
+            </Link>
+            !
+          </p>
         </div>
       </section>
+
       <section className="mt-10" id="timeline">
         <h1 className="section-title">Timeline</h1>
 
