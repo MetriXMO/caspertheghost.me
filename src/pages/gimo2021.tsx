6d12111dc63f454194962ad6f978029f9f06caa5
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-19 20:53:37 +0530

    Update gimo2021.tsx

--- src/pages/gimo2021.tsx
+++ src/pages/gimo2021.tsx
@@ -1,9 +1,14 @@
 import { Project } from "types/Project";
 import { Layout } from "components/Layout";
+import { GetStaticProps } from "next";
+import { FeaturedProjectsSection } from "components/FeaturedProjects";
+import { Project } from "types/Project";
 
+interface Props {
+  projects: Project[];
+}
 
-
-export default function gimo2021() {
+export default function gimo2021({projects }: Props) {
   return (
     <Layout>
       
@@ -13,5 +18,13 @@ export default function gimo2021() {
   );
 }
 
+export const getStaticProps: GetStaticProps = async () => {
+  const projects = (await import("data/featured-projects")).projects;
 
+  return {
+    props: {
+      projects,
+    },
+  };
+};
 
