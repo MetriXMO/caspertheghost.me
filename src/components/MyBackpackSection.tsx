3a595d2d992ceae2ae87ca31093f5d60c9e3c95c
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-20 11:11:32 +0530

    Update MyBackpackSection.tsx

--- src/components/MyBackpackSection.tsx
+++ src/components/MyBackpackSection.tsx
@@ -4,10 +4,10 @@ export const MyBackpackSection = ({ myBackpack }: { myBackpack: [string, string[
       <h1 className="section-title">GIMO 2022 Team</h1>
 
       <p className="my-4 text-neutral-800 dark:text-gray-300">
-        You can find our team here. TOur team consists of multiple IMO contestants who have won 
-<b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">
+       The team consists of <b>multiple IMO contestants</b> who have won <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">
   gold, silver and bronze</b> in IMO,
- some who are going to partcipate in IMO this year representing their country, MOP ers, USA(J)MO qualifiers and math enthusisats.
+        some who are going to <b>partcipate in IMO this year</b> representing their country, <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">MOP</b> ers,
+        <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">USA(J)MO qualifiers</b> and <bclassName="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">math enthusisats</b>.
       </p>
 
       <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3">
