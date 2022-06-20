6fc7b2c90b18a4bb822e5cbb3ff3907d53deee99
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-20 09:24:48 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -17,7 +17,7 @@ export const HeroSection = () => {
         IMO contestants who have won gold, silver and bronze in IMO for their country, some who are going to partcipate in IMO this year representing their country,
         MOP ers, USA(J)MO qualifiers and math enthusiasts. Here you will be given nearly ten  days to work out with six problems and you have submit them within that time interval. To know more, check the about page. 
       
-      </p>
+      </p>  
     </section>
   );
 };
