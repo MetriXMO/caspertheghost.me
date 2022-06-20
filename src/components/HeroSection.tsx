79112fe9d98db6e59b7d91c38c2193caff9d5581
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-06-20 11:28:03 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -13,9 +13,9 @@ export const HeroSection = () => {
       </div>
 
       <p className="max-w-3xl mt-8 text-neutral-800 dark:text-gray-300 md:text-xl">
-        GIMO is an International Mathematical Olympiad Style mathematical contest held online for individuals by GIMO team which consists of multiple 
-        IMO contestants who have won gold, silver and bronze in IMO for their country, some who are going to partcipate in IMO this year representing their country,
-        MOP ers, USA(J)MO qualifiers and math enthusiasts. Here you will be given nearly ten  days to work out with six problems and you have submit them within that time interval. To know more, check the about page. 
+        GIMO is an International Mathematical Olympiad Style mathematical contest held online for individuals by GIMO team consists of <b>multiple IMO contestants</b> who have won <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#228cff] md:text-[#228cff]">
+  gold, silver and bronze</b> in IMO,
+        some who are going to <b>partcipate in IMO this year</b> representing their country, <b>MOP qualifiers</b>, <b>USA(J)MO qualifiers</b> and <b>math enthusisats</b>. Here you will be given nearly ten  days to work out with six problems and you have submit them within that time interval. To know more, check the about page. 
       
       </p>  
     </section>
