831a2ea4de325ec196fe63b57eeb0edb2143a50f
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-07-27 23:07:27 +0530

    Update my-backpack.ts

--- src/data/my-backpack.ts
+++ src/data/my-backpack.ts
@@ -5,6 +5,7 @@ export const myBackpack: [string, string[]][] = [
       "Kaixin Wang, Canada",
       "Agamjeet Singh, India",
       "Bora Olmez, Switzerland",
+      "Pang Cheng Wu, Taiwan",
       "Atul Nadig, India",
       "Fedir Yudin, Ukraine",
       "Andrei Chirita, Romania",
@@ -18,8 +19,8 @@ export const myBackpack: [string, string[]][] = [
     ],
   ],
   [" ", ["Papon Lapate, Thailand", "Aritra Mondal, India", "Gabriel, Singapore", 
-               "Arkan Manva, India" , "Vlad Spataru, Romania" , "Vedant Saini, India" ,
+              ,"Behanorm", "Arkan Manva, India" , "Vlad Spataru, Romania" , "Vedant Saini, India" ,
                "Mattedy" ,"Valentino Iversion, Indonesia" ,"Anand, India" ,"NJOY, India" ,"Sunaina Pati, India" ]],
   
-  [" ", ["Orestis Lignos, Greece", "Jimin Kim, Korea","Evan Chang", "Krishna Prothapragada, USA", "k12byda5h, Thailand", "Andre Luis, Bolivia", "navi_09220114, Malaysia", "Justin Lee, USA", "Rafael Yanto", "Miquel-point", "player_01"]],
+  [" ", ["Orestis Lignos, Greece", "Jimin Kim, Korea","Evan Chang, USA", "Krishna Prothapragada, USA", "k12byda5h, Thailand","mela_20-15, Greece", "Andre Luis, Bolivia", "navi_09220114, Malaysia", "Justin Lee, USA", "Rafael Yanto", "Miquel-point", "player_01"]],
 ];
