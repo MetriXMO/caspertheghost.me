96c23f71c680493a0f44f835e9a6269128137687
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-07-27 23:45:07 +0530

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -38,27 +38,31 @@ export default function About() {
           <br></br>
 <p> Each problem is worth 7 points so you can score at most 42 points. Please use wisely the time we gave to workout with the problems.
   Contest Format is individual and <b>detailed schedule</b> is provided below and on the PDF. </p>
-          <p>Submission Process</p><br></br>
           
-          <p> 
-            <b>Submission Process I.</b> You can submit your solutions on AoPS PM to <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Aritra/Aritra 12</b> on AoPS  if you have an account at AoPS. This is the most preferred way of submission and it is also beneficial
-to participants because on AoPS PM you are allowed to send solutions one by one in that single PM, however you are not allowed so for the other two processes. But obviously you can send day 1
-and day 2 separately. <br></br>
-
-            <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Submission Process II.</b> For submitting Day I Solutions please upload solutions in the following link:
-            <br></br>
-            <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Submission Process III.</b> If you are unable to do any of the things above then just simply mail your
-solutions pdf to us on our mail <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">gaussiancurv180@gmail.com</b>
-
-Submit you solutions through the form given in pdf below. There are other ways of submission too which you can find in the pdf.</p>
+          
           
            <br></br>
             <h1 className="section-title">GIMO Schedule</h1>
           
           <p className="mt-5">
-           The contest will be tentatively held on August last week or september first week.
+           The contest will be held on 21 th August, 2022 and will end on 1st September, 2022. There is no registration deadline. 
           </p>
         
+          
+          
+       <h1 className="section-title"GIMO Submission Process</h1>
+          
+          <p> 
+            <b>Submission Process I.</b> You can submit your solutions on AoPS PM to <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Aritra/Aritra 12</b> on AoPS  if you have an account at AoPS. This is the most preferred way of submission and it is also beneficial
+to participants because on AoPS PM you are allowed to send solutions one by one in that single PM, however you are not allowed so for the other two processes. But obviously you can send day 1
+and day 2 separately. <br></br>
+          </p>
+            <p><b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Submission Process II.</b> For submitting Day I Solutions please upload solutions in the following link:
+          </p>   <br></br>
+         <p>   <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">Submission Process III.</b> If you are unable to do any of the things above then just simply mail your
+solutions pdf to us on our mail <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">gaussiancurv180@gmail.com</b></p>
+
+          
         </div>
       </section>
 
