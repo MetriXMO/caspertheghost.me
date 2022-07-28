24683f6fb698cd8d9d82d084fde775438ce45bcd
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-07-28 20:35:02 +0530

    Update team.tsx

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -1,8 +1,11 @@
 import { Layout } from "components/Layout";
 import { MyBackpackSection } from "components/MyBackpackSection";
 import { GetStaticProps } from "next";
+import { NextSeo } from "next-seo";
 
 const pageTitle = "GIMO Team";
+const pageDescription = "GIMO Team"
+
 interface Props {
 
   myBackpack: [string, string[]][];
@@ -11,7 +14,12 @@ interface Props {
 export default function team({ myBackpack}: Props) {
   return (
     <Layout>
-     
+      <NextSeo
+        openGraph={{ title: pageTitle, description: pageDescription }}
+        canonical="https://hellogimo.vercel.app/team"
+        title={pageTitle}
+        description={pageDescription}
+      />
       <MyBackpackSection myBackpack={myBackpack} />
      
     </Layout>
