4d5fcfe60777ed659d5d3c9b9b414636efa455e8
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-08-03 17:23:12 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://caspertheghost.me",
+  siteUrl: "https://hellogimo.vercel.app/",
   generateRobotsTxt: true,
   sitemapBaseFileName: "sitemap",
 };
