bf441d4b621d3ce7ba3877d9af40c041b81b57c3
MetriXMO <86888479+MetriXMO@users.noreply.github.com>
2022-08-04 19:49:33 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -20,8 +20,8 @@ export const HeroSection = () => {
       <p className="max-w-3xl mt-8 text-neutral-800 dark:text-gray-300 md:text-xl">
         GIMO is an International Mathematical Olympiad style mathematical contest held online every year for individuals. It is organised by the <b>GIMO team</b> which <b>consists</b> of <b>multiple IMO contestants</b> who have won <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">
   gold, silver and bronze</b> in IMO, <b>MOP qualifiers</b>, <b>USA(J)MO qualifiers</b> and <b>math enthusisats</b>. You will be provided nearly twelve days to work out with six problems and you have to submit your response within that time interval. To know more, check the about page. 
-        
-        Here is the discord server where daily GIMO updates will be provided: <href="https://discord.gg/RdNHVKhgGP">GC Discord Server</href>
+        <br></br><br></br>
+        Here is the <b className="max-w-3xl mt-8 text-neutral-800 dark:text-[#14deb9] md:text-[#262627]">discord server</b> where daily GIMO updates will be provided: <href="https://discord.gg/RdNHVKhgGP">GC Discord Server</href>
       
       </p>  
     </section>
